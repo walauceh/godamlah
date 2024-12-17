35e9a70913267d0810dac24cf321731f7c3ee3ae
walauceh <145186203+walauceh@users.noreply.github.com>
2024-12-17 17:51:06 +0800

    final fixes for vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true, // Recommended for modern React apps
-  swcMinify: true,       // Use SWC for faster builds
-  output: undefined,     // Standard output, not static export
+  output: 'export'
 };
 
 export default nextConfig;
-
