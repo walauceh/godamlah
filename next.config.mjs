0c02e37afecfdc622db6d09eb79284d3a2b3386a
walauceh <145186203+walauceh@users.noreply.github.com>
2025-01-11 05:33:44 +0800

    final deployment fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    reactStrictMode: true
+    reactStrictMode: true,
+    output: undefined,
+    swcMinify: true,
 };
 
 export default nextConfig;
